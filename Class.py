ab5d8cecb10563b6d0c21c30f56725ed553b8498
jerry2793 <67173020+jerry2793@users.noreply.github.com>
2020-12-06 12:35:47 -0800

    Update Class.py

--- Class.py
+++ Class.py
@@ -1,5 +1,6 @@
 help(dir)
 class SomeClass:
+    # use self.somevar statement for bind it to the class storage and make it a class variable accessible like this: classname.statement
     astring = "abc"
     anumber = 123    
     def Display(self):
